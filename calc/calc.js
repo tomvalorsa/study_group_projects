45d7143f3354dac29b26675923cc96ed3d633e82
Tom Valorsa <tomvalorsa@gmail.com>
2015-10-07 19:14:33 +1100

    Basic calc working

--- calc/calc.js
+++ calc/calc.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
+  $('.square').each(function(i, square) {
+    // fix this to alternate colour on rows
+    var bgColor = i % 2 === 0 ? '#ABA4AD' : '#8E878F';
+    $(square).css({'background-color': bgColor});
+  });
+
   var calculator = {
-    add: function(a, b) { console.log(a + b) },
-    subtract: function(a, b) {console.log(a - b) },
-    divide: function(a, b) { console.log(a / b) },
-    multiply: function(a, b) { console.log(a * b) },
+    add: function(a, b) { return (a + b) },
+    subtract: function(a, b) { return (a - b) },
+    divide: function(a, b) { return (a / b) },
+    multiply: function(a, b) { return (a * b) },
     memory: []
   }
 
@@ -12,43 +18,57 @@ $(document).ready(function() {
     $('#screen')[0].innerText = '';
   }
 
+  var addToMemory = function(buttonVal) {
+    $('#screen')[0].innerText += buttonVal;
+    calculator.memory.push(buttonVal);
+  }
+
   var makeCalculation = function() {
-    // take the array, look for operands and then do the calc
-    console.log(calculator.memory);
+    // Improve so it can perform operations on more than two numbers
     var numA = parseInt(calculator.memory[0]),
       operation = calculator.memory[1],
-      numB = parseInt(calculator.memory[2]);
+      numB = parseInt(calculator.memory[2]),
+      result
 
     switch(operation) {
       case '+':
-        return calculator.add(numA, numB);
+        result = calculator.add(numA, numB);
         break;
       case '-':
-        return calculator.subtract(numA, numB);
+        result = calculator.subtract(numA, numB);
         break;
       case '/':
-        return calculator.divide(numA, numB);
+        result = calculator.divide(numA, numB);
         break;
       case '*':
-        return calculator.multiply(numA, numB);
+        result = calculator.multiply(numA, numB);
         break;
     }
+
+    $('#screen')[0].innerText = result;
+    calculator.memory.push('=');
   }
 
   $('.btn').on('click', function() {
     var buttonVal = this.innerHTML;
-    console.log(buttonVal);
 
-    if (buttonVal === 'CLEAR') {
-      return clearMemory();
+    if (calculator.memory[calculator.memory.length -1] === '=') {
+      clearMemory();
     }
 
-    if (buttonVal === '=') {
-      return makeCalculation();
+    switch(buttonVal) {
+      case 'CLEAR':
+        return clearMemory();
+        break;
+      case '=':
+        return makeCalculation();
+        break;
+      case '.':
+        // account for decimal points
+        break;
+      default:
+        addToMemory(buttonVal);
+        break;
     }
-
-    $('#screen')[0].innerText += buttonVal;
-    calculator.memory.push(buttonVal);
   });
-
 });
